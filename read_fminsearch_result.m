f97b2aa8ee4da9a6cd76c706122f43103105d431
Jiayin Hong <hongjy.pi@gmail.com>
2017-05-10 21:22:43 -0400

    add iteration as output of function 'read_fminsearch_result'
    and fix the bug that when the last line of the file is ‘done’, it
    cannot be read and processed correctly

--- read_fminsearch_result.m
+++ read_fminsearch_result.m
@@ -1,4 +1,4 @@
-function [param, obj] = read_fminsearch_result(filepath)
+function [param, obj, iter] = read_fminsearch_result(filepath)
 
 fid = fopen(filepath);
 if fid == -1
@@ -7,15 +7,23 @@ end
 
 % read the txt file, and use the new line to replace the old one
 % until the end of the file
+line = fgetl(fid);
+line_minus_one = [];
 while ~feof(fid)
+    line_minus_two = line_minus_one;
+    line_minus_one = line;
     line = fgetl(fid);
 end
 
 % convert the last line of the file into a one-row cell array,
 % where each column is splited by tab
-line = regexp( deblank(line), '\t', 'split' );
+if strcmp(line, 'done')
+    line = line_minus_two;
+end
 
-obj = str2num(line{2});
+line = regexp( deblank(line), '\t', 'split' );
+iter = str2double(line{1});
+obj = str2double(line{2});
 param = cellfun( @str2num, line(4:end) );
 
 end
