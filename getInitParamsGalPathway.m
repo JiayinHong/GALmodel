df0b25a0162280de66e666ed62e48139dd5e73bd
Jiayin Hong <hongjy.pi@gmail.com>
2017-08-15 14:34:18 -0400

    Change the equations
    Make the current in use equations to be GALode3, which delete ‘Mig1s’
    from 12 species and use a fraction of Mig1 total instead. Updating
    ‘getInitParamsGalPathway’ correspondingly, as there are only 11 species
    now.

--- getInitParamsGalPathway.m
+++ getInitParamsGalPathway.m
@@ -2,7 +2,8 @@ function output = getInitParamsGalPathway( param )
 % this function is used to get the initial value for ON/OFF state
 
 load_global;
-opt = odeset('NonNegative',1:12);
+% opt = odeset('NonNegative',1:12);
+opt = odeset('NonNegative',1:11);
 accurate_thresh = 10^-8;
 % opt = [];
 % opt = odeset('AbsTol', 1e-12, 'RelTol', 1e-12);
@@ -10,17 +11,26 @@ accurate_thresh = 10^-8;
 % generate seed param for starting from GLUCOSE only state
 % param.exglu = 0.25*perc_to_nm;
 param.exglu = 2*perc_to_nm;     % to solve the bifurcation problem
-% param.exglu = 4*perc_to_nm;
 param.exgal = 0;
-odefunc = @(t,y)GALode(t,y,param);
+
+% The Current Equations In Use Is GALode3!
+odefunc = @(t,y)GALode3(t,y,param);
+
 % when exgal==0 -> Gal3*=0, C83=0, gal=0
-tmp = ones(1,12);
+% tmp = ones(1,12);
+% tmp(1) = 0;     % Gal1
+% tmp(6) = 0;     % Gal3*
+% tmp(9) = 0;     % C83
+% tmp(12) = 0;    % intracellular galactose
+
+% delete R*, there are only 11 species
+tmp = ones(1,11);
 tmp(1) = 0;     % Gal1
 tmp(6) = 0;     % Gal3*
-tmp(9) = 0;     % C83
-tmp(12) = 0;    % intracellular galactose
-% tmp = [0, ones(1,4),0,1,1,0,1,1,0];
+tmp(8) = 0;     % C83
+tmp(11) = 0;    % intracellular galactose
 [~, y] = ode15s( odefunc, [0 10000], tmp, opt);
+
 y(y<accurate_thresh) = 0;   % omit values that are too small
 y0_Glu=y(end,:);
 
@@ -28,15 +38,23 @@ y0_Glu=y(end,:);
 param.exglu = 0;
 % param.exgal = 0.25*perc_to_nm;
 param.exgal = 2*perc_to_nm;     % i.e. sometimes the local and cluster got different results
-% param.exgal = 4*perc_to_nm;
-odefunc = @(t,y)GALode(t,y,param);
+
+% The Current Equations In Use Is GALode3!
+odefunc = @(t,y)GALode3(t,y,param);
+
 % when exglu==0 -> R*=0, glu=0
-tmp = ones(1,12);
+% tmp = ones(1,12);
+% tmp(1) = 0;     % Gal1
+% tmp(8) = 0;     % R*
+% tmp(11) = 0;    % intracellular glucose
+
+% delete R*, there are only 11 species
+tmp = ones(1,11);
 tmp(1) = 0;     % Gal1
-tmp(8) = 0;     % R*
-tmp(11) = 0;    % intracellular glucose
-% tmp = [0, ones(1,6),0,1,1,0,1];
+tmp(10) = 0;    % intracellular glucose
+
 [~, y] = ode15s( odefunc, [0 10000], tmp, opt);
+
 y(y<accurate_thresh) = 0;   
 y0_Gal=y(end,:);
 
