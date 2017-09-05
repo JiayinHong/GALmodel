b39e8348e9bf4bd2034fe77b6e3e8747f3e969f5
Jiayin Hong <hongjy.pi@gmail.com>
2017-09-05 11:32:16 -0400

    add an option in 'evalGalPathway'
    user can choose the ode solver from matlab ode15s and fortran ode
    solver now.

--- evalGalPathway.m
+++ evalGalPathway.m
@@ -1,37 +1,32 @@
-function output = evalGalPathway( param, trait, fit_type )
+function output = evalGalPathway( param, trait, fit_type, solver )
 %   the third version of eval_param, aim to increase the compatibility
 %   when dealing with multiple rows and columns of data
+fit_type_config;
+trait = trait(index_list,:);
 
-output = getInitParamsGalPathway(param);
-
-if nargin == 3
-    
-%     switch fit_type
-%         case 'one_row'
-%             index_list = [4:8:92];
-%         case 'one_column'
-%             index_list = [65:72];
-%         case 'one_cross'
-%             index_list = [4:8:60,65:72,76,84,92];
-%         case 'single_gradient'
-%             index_list = [1:12];
-%     end
-    fit_type_config;
-
-    trait = trait(index_list,:);
+if nargin < 4   % to be compatible with previous version
+    solver = 'ode15s';  % if not specify, use matlab ode15s
 end
-%%%%%%%%%%%%%%%%%
-% output.y0_Glu = [0.9411 7.0453e-05 111.4866 0.1766 152.0683 0 0 0 0 22.9261 5.5329e+03 0];
-% output.y0_Gal = [0.9411 114.0955 35.8294 20.9579 0.1199 37.8327 0 0 196.7760 2.1448 0 35.0351];
-%%%%%%%%%%%%%%%%
-y_ss_Glu = evalMultiSugarConcentrations( param, output.y0_Glu, trait.gluc, trait.galc );
-y_ss_Gal = evalMultiSugarConcentrations( param, output.y0_Gal, trait.gluc(end:-1:1), trait.galc(end:-1:1) );
+
+switch solver
+    case 'ode15s'
+        output = getInitParamsGalPathway(param);
+        y_ss_Glu = evalMultiSugarConcentrations( param, output.y0_Glu, trait.gluc, trait.galc );
+        y_ss_Gal = evalMultiSugarConcentrations( param, output.y0_Gal, trait.gluc(end:-1:1), trait.galc(end:-1:1) );
+        
+    case 'fortran'
+        output = FgetInitParamsGalPathway(param);
+        y_ss_Glu = FevalMultiSugarConcentrations( param, output.y0_Glu, trait.gluc, trait.galc );
+        y_ss_Gal = FevalMultiSugarConcentrations( param, output.y0_Gal, trait.gluc(end:-1:1), trait.galc(end:-1:1) );
+        
+end
+
 y_ss_Gal = y_ss_Gal(end:-1:1,:);
 
 % y_ss_Glu_init = evalMultiSugarConcentrations( param, output.y0_Glu, trait.gluc(1), trait.galc(1) );
 % y_ss_Gal_init = evalMultiSugarConcentrations( param, output.y0_Gal, trait.gluc(end), trait.galc(end) );
 % y_ss_Gal_init(1) = y_ss_Glu_init(1);
-% 
+%
 % y_ss_Glu = evalMultiSugarConcentrations( param, y_ss_Glu_init, trait.gluc(2:end), trait.galc(2:end) );
 % y_ss_Glu = [y_ss_Glu_init; y_ss_Glu];
 % y_ss_Gal = evalMultiSugarConcentrations( param, y_ss_Gal_init, trait.gluc(end-1:-1:1), trait.galc(end-1:-1:1) );
@@ -42,11 +37,15 @@ induction_level = y_ss_Gal(:,1);
 
 [output.sum_obj, output.simulation_result_linear, output.experiment_result_linear] = calculate_obj( trait, basal_level, induction_level );
 
-% autofluorescence = get_auto_fluorescence(trait);
-% output.all_conc_Glu = y_ss_Glu + autofluorescence; 
-% output.all_conc_Gal = y_ss_Gal + autofluorescence; 
+autofluorescence = get_auto_fluorescence(trait);
+% output.all_conc_Glu = y_ss_Glu + autofluorescence;
+% output.all_conc_Gal = y_ss_Gal + autofluorescence;
 output.all_conc_Glu = y_ss_Glu; % all 12 variables concentration at steady state, initial from Glu only condition
 output.all_conc_Gal = y_ss_Gal; % all 12 variables concentration at steady state, initial from Gal only condition
+
+% for GAL1 level, add autofluorescence
+output.all_conc_Glu(:,1) = output.all_conc_Glu(:,1) + autofluorescence;
+output.all_conc_Gal(:,1) = output.all_conc_Gal(:,1) + autofluorescence;
 
 end
 
