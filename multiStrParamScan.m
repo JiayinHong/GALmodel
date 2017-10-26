5af3eb338b01f7c7bc7fdaa54e602b0051479d72
Jiayin Hong <hongjy.pi@gmail.com>
2017-10-26 14:06:21 -0400

    update 'multiStrParamScan'
    modify the repeated codes to in-script functions, now this script can
    be applied either to multi strain single parameter scan (i.e. Kayla’s
    data) or to simulation generated fake traits

--- multiStrParamScan.m
+++ multiStrParamScan.m
@@ -12,7 +12,9 @@ for i=3:length(tmp)     % the first 2 are not files
         str = fieldnames(obj);  % fieldnames is a cell
         str = str{1};           % convert cell to char
         no = str(4:end);        % 'str10' -> '10'
-        no = str2num(no)-2;     % '10' -> 8
+        %         no = str2num(no)-2;     % '10' -> 8
+        no = str2num(no)-3;     % be careful to -2 / -3,
+        % if there's '.DS_Store' in the traits folder, should -3
         fdn = sprintf('str%02d',no);    % fdn='str08'
         obj_all.(fdn) = obj.(str);  % assign the value to the same field of obj_all
     end
@@ -20,83 +22,62 @@ end
 
 %% store all the strain names into a cell array
 clear strNames
-strNames = cell(35,1);
+nStr = numel(fieldnames(obj_all));
+strNames = cell(nStr,1);
+
 for i=3:length(allTraits)   % the first 2 are not traits
-    tmp = allTraits(i).name;
-    strNames{i-2} = tmp(1:end-4);   % to remove '.mat'
+    if ~ strcmp(allTraits(i).name, '.DS_Store')
+        tmp = allTraits(i).name;
+        %         strNames{i-2} = tmp(1:end-4);   % to remove '.mat'
+        strNames{i-3} = tmp(1:end-4);   % to remove '.mat'
+    end
 end
 
-% load base parameters (best-fit) of wildtype
+%% load base parameters (best-fit) of wildtype
 load('../wildtype_96well-025-170925_16:18.mat')
 param_names = parameter_update.parameter_name;
 base_param = param_map;
 
+% strain sets for data from Kayla (multi strain comparison)
+% strainSet1 = {'str13','str22','str08','str10','str16'};
+% strainSet2 = {'str34','str23','str26','str27'};
+% strainSet3 = {'str01','str02','str11','str12','str29'};
+% strainSet4 = {'str32','str05','str20'};
+% strainContainNaN = {'str04','str06','str19','str31','str33'};
+% outpath = '../multiStrScan/';
+
+% strain sets for simulation generated fake traits
+for i=1:8
+    MMconstAct{i} = sprintf('str%02d',i);
+end
+for i=9:16
+    MMconstRep{i-8} = sprintf('str%02d',i);
+end
+for i=17:26
+    basalSynthesis{i-16} = sprintf('str%02d',i);
+end
+for i=27:34
+    induceSynthesis{i-26} = sprintf('str%02d',i);
+end
+for i=35:40
+    Others{i-34} = sprintf('str%02d',i);
+end
+for i=41:48
+    bindRate{i-40} = sprintf('str%02d',i);
+end
+for i=49:58
+    hillCoeff{i-48} = sprintf('str%02d',i);
+end
+
 %% plot multi-strains single param scan on one fig
-if 0
-    obj_thresh = 10;    % to draw a reference line
-    strainSet1 = {'str13','str22','str08','str10','str16'};
-    strainSet2 = {'str34','str23','str26','str27'};
-    strainSet3 = {'str01','str02','str11','str12','str29'};
-    strainSet4 = {'str32','str05','str20'};
-    strainContainNaN = {'str04','str06','str19','str31','str33'};
-    outpath = '../multiStrScan/';
-    
+if 1
+    outpath = '../fakeTraitsScan/';
+    strainSet = hillCoeff;
+    setName = 'hillCoeff';
     for i_param = 1:length(param_names)
         param_name = param_names{i_param};
         base_val = base_param.(param_name);
-        
-        if regexp(param_name, 'n*')     % hill coefficients
-            perturb_coefficient = linspace(1,4,7);
-            nValue = length(perturb_coefficient);
-            varied_value = perturb_coefficient;
-            catnames = cell(1,nValue);
-            for i=1:nValue
-                catnames{i} = num2str(varied_value(i), '%.1f');
-            end
-            
-        else
-            perturb_coefficient = logspace(log10(0.001), log10(1000), 13);
-            nValue = length(perturb_coefficient);
-            varied_value = perturb_coefficient .* base_val;
-            catnames = cell(1,nValue);
-            for i=1:nValue
-                catnames{i} = num2str(varied_value(i), '%.4f');
-            end
-            
-        end
-        
-        figure
-        set(gcf, 'position', [2049 418 1065 411])
-        iStr=[];
-        j=1;
-        for fdn = strainContainNaN                % select a couple of strains to draw on the same plot
-            tmp = obj_all.(fdn{1});
-            plot(1:nValue, tmp(i_param,1:nValue), 'LineWidth', 1.5, 'Marker', 'o')
-            iStr(j) = str2num(fdn{1}(4:end));   % iStr stores the strains that have been drawn
-            j=j+1;
-            hold all
-        end
-        
-        hline = refline([0 obj_thresh]);    % add a reference line
-        hline.LineWidth = 1.5;
-        hline.Color = 'm';
-        hline.LineStyle = '-.';
-        
-        set(gca, 'xtick', 1:nValue, 'xticklabels', catnames)
-        if regexp(param_name, 'n*')     % hill coefficients
-            % no rotation
-        else
-            set(gca, 'XTickLabelRotation', 45)
-        end
-        grid on
-        legend(strNames(iStr), 'location', 'NorthWest')
-        xlabel(sprintf('%s param values', param_name))
-        ylabel('sum obj - 96 well')
-        set(gca, 'YScale', 'log')
-        set(gca, 'FontSize', 12, 'FontWeight', 'bold', 'FontName', 'Helvetica')
-        
-        export_fig(fullfile(outpath, sprintf('%s vary in logspace - SetNaN', param_name)));
-        
+        multiStrParamScanHelper(param_name, strNames, base_val, obj_all, i_param, outpath, strainSet, setName)
     end
 end
 
@@ -108,6 +89,68 @@ for i=3:length(allTraits)
     target_trait = trait;
     
     plot_heatmap(filename, target_trait)
+end
+
+%%  below is in-script functions
+
+function multiStrParamScanHelper(param_name, strNames, base_val, obj_all, i_param, outpath, strainSet, setName)
+
+obj_thresh = 10;    % to draw a reference line
+cmap = lines(7);    % customize a colormap, the default colomap for lines is 'lines' which contains 7 unique colors
+cmap(8:10,:) = winter(3);   % in our case, it's not sufficient, so I add three more colors from other colormaps
+cmap(9,:) = autumn(1);  % for more details and options, see manual of 'colormap'
+
+if regexp(param_name, 'n*')     % hill coefficients
+    perturb_coefficient = linspace(1,4,7);
+    nValue = length(perturb_coefficient);
+    varied_value = perturb_coefficient;
+    catnames = cell(1,nValue);
+    for i=1:nValue
+        catnames{i} = num2str(varied_value(i), '%.1f');
+    end
+    
+else
+    perturb_coefficient = logspace(log10(0.001), log10(1000), 13);
+    nValue = length(perturb_coefficient);
+    varied_value = perturb_coefficient .* base_val;
+    catnames = cell(1,nValue);
+    for i=1:nValue
+        catnames{i} = num2str(varied_value(i), '%.4f');
+    end
+    
+end
+
+figure
+set(gcf, 'position', [2049 418 1065 411])
+whichStr=[];
+j=1;
+for fdn = strainSet                % select a couple of strains to draw on the same plot
+    tmp = obj_all.(fdn{1});
+    plot(1:nValue, tmp(i_param,1:nValue), 'Color', cmap(j,:), 'LineWidth', 1.5, 'Marker', 'o')
+    whichStr(j) = str2num(fdn{1}(4:end));   % iStr stores the strains that have been drawn
+    j=j+1;
+    hold all
+end
+
+hline = refline([0 obj_thresh]);    % add a reference line
+hline.LineWidth = 1.5;
+hline.Color = 'm';
+hline.LineStyle = '-.';
+
+set(gca, 'xtick', 1:nValue, 'xticklabels', catnames)
+if regexp(param_name, 'n*')     % hill coefficients
+    % no rotation
+else
+    set(gca, 'XTickLabelRotation', 45)
+end
+grid on
+legend(strNames(whichStr), 'location', 'NorthWest')
+xlabel(sprintf('%s param values', param_name))
+ylabel('sum obj - 96 well')
+set(gca, 'YScale', 'log')
+set(gca, 'FontSize', 12, 'FontWeight', 'bold', 'FontName', 'Helvetica')
+
+export_fig(fullfile(outpath, sprintf('%s vary in logspace - %s', param_name, setName)));
 end
 
 function plot_heatmap(filename, target_trait, param_set)
